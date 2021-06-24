424be17a3e6139dd605711756d884c2fe1532697
MajorKuprich <majorkuprich@gmail.com>
2021-06-24 18:59:12 +0200

    Create MultiSourceValidatorTest.php

---
+++ Test/Unit/MultiSourceValidatorTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BP\Checkout\Test\Unit;
+
+use MK\Module\Service\MultiSourceService;
+use MK\Module\Validator\MultisourceValidator;
+use Magento\Framework\Exception\ValidatorException;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Quote\Model\Quote;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionProperty;
+
+class MultiSourceValidatorTest extends TestCase
+{
+    public function testValidate(): void
+    {
+        $quoteMock = $this->getMockBuilder(Quote::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $multiSourceServiceMock = $this->getMockBuilder(MultiSourceService::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $multiSourceServiceMock
+            ->method('isMultisource')
+            ->willReturn(true);
+        $validatorMock = $this->getMockBuilder(MultisourceValidator::class)
+            ->setConstructorArgs([$multiSourceServiceMock])
+            ->getMock();
+
+        self::assertTrue($multiSourceServiceMock->isMultisource($quoteMock));
+
+        $objectManager = new ObjectManager($this);
+        $validator = $objectManager->getObject(MultisourceValidator::class, [$multiSourceServiceMock]);
+        $this->setAccessibleProperties($validator, [$multiSourceServiceMock]);
+
+        $this->expectException(ValidatorException::class);
+
+        $validator->validate($quoteMock);
+
+//        $validatorMock->validate($quoteMock);
+    }
+
+    private function setAccessibleProperties(object $object, array $properties): void
+    {
+        $refClass = new ReflectionClass(get_class($object));
+        $refProperties = $refClass->getProperties(ReflectionProperty::IS_PRIVATE);
+        foreach ($refProperties as $key => $refProperty) {
+            $refProperty->setAccessible(true);
+            $refProperty->setValue($object, $properties[$key]);
+        }
+    }
+}
